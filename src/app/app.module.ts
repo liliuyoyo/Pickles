b2d96cc275e375597ad8a5751f9f6e1b9f26234d
liyooo <liliuyoyo@gmail.com>
2019-03-18 13:57:06 -0500

    components structure 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './partials/header/header.component';
+import { FooterComponent } from './partials/footer/footer.component';
+import { SliderComponent } from './partials/slider/slider.component';
+import { MoviesListComponent } from './movies-list/movies-list.component';
+import { FilterComponent } from './movies-list/filter/filter.component';
+import { MovieComponent } from './movies-list/movie/movie.component';
+import { MovieItemComponent } from './movies-list/movie-item/movie-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SliderComponent,
+    MoviesListComponent,
+    FilterComponent,
+    MovieComponent,
+    MovieItemComponent
   ],
   imports: [
     BrowserModule
