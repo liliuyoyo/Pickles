1c9ca14747796afd32f78be7aac3bef98db08730
liyooo <liliuyoyo@gmail.com>
2019-03-20 09:41:34 -0500

    add unsubsribe

--- client/src/app/movies-list/movies-list.component.ts
+++ client/src/app/movies-list/movies-list.component.ts
@@ -1,24 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Observable } from  "rxjs/Observable";
 import { Movie } from '../models/movie.model';
 import { MoviesService } from '../services/movies.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-movies-list',
   templateUrl: './movies-list.component.html',
   styleUrls: ['./movies-list.component.css']
 })
-export class MoviesListComponent implements OnInit {
-
+export class MoviesListComponent implements OnInit, OnDestroy {
+  private subscription : Subscription;
   moviesList: Movie[];
 
   constructor(private moviesService: MoviesService) {}
 
   ngOnInit() {
-    this.moviesService.getMovies()
+    this.subscription = this.moviesService.getMovies()
     .subscribe(data => {
       this.moviesList = data;
     });;
   }
 
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
+  }
 }
--- client/src/app/services/movies.service.ts
+++ client/src/app/services/movies.service.ts
@@ -7,28 +7,6 @@ import { Movie } from '../models/movie.model';
 @Injectable()
 export class MoviesService {
     
-    private movies: Movie[];//= [
-    //     new Movie("Harry Potter",
-    //             "Love this movie...",
-    //             "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQSDXEJwBLV-yzjNOFHMoJ-OqSyFtjjqweTkvby3rePZYOzudM",
-    //             2008,
-    //             "aaa",
-    //             ["bbb","ccc"],
-    //             "fantsy",
-    //             "UK",
-    //             120,
-    //             10), 
-    //     new Movie("Harry Potter1",
-    //             "Love this movie too...",
-    //             "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVCFC_uuZY7jIT7EecrWAvYYpsFIubVyznbtju4ve50o_AYenZ",
-    //             2010,
-    //             "aaa",
-    //             ["bbb","ccc"],
-    //             "fantsy",
-    //             "UK",
-    //             120,
-    //             10)];
-
     serverUrl: string = "http://localhost:4600/";
 
     constructor(private http: HttpClient){};
