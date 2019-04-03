4f4487fc0c0b6bdad734526b4155c59d39c5c77e
liyooo <liliuyoyo@gmail.com>
2019-04-03 12:04:24 -0500

    show username when loggin

--- client/src/app/partials/header/header.component.ts
+++ client/src/app/partials/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MoviesService } from 'src/app/services/movies.service';
 import { Router } from '@angular/router';
 import { UserService } from 'src/app/services/user.service';
+import { User } from 'src/app/models/user.model';
 
 @Component({
   selector: 'app-header',
@@ -10,6 +11,7 @@ import { UserService } from 'src/app/services/user.service';
 })
 export class HeaderComponent implements OnInit {
   searchString:string;
+  user:User;
 
   constructor(private moviesService : MoviesService,
               private userService: UserService,
@@ -26,6 +28,7 @@ export class HeaderComponent implements OnInit {
   }
 
   public isLoggedin(){
+    // get username from server
     return this.userService.getToken() !== null;
   }
 
