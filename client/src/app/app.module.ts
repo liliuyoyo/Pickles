87ab9021bfc475ac66bca380365f830f6cefa8d5
liyooo <liliuyoyo@gmail.com>
2019-04-04 16:26:38 -0500

    minor

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -28,12 +28,8 @@ import { SignupComponent } from './users/signup/signup.component';
 import { LoginComponent } from './users/login/login.component';
 import { AddCommentComponent } from './movie-detail/add-comment/add-comment.component';
 import { WelcomeComponent } from './users/welcome/welcome.component';
-<<<<<<< HEAD
 import { LoginPopupComponent } from './users/login-popup/login-popup.component';
-||||||| merged common ancestors
-=======
 import { UserProfileComponent } from './users/user-profile/user-profile.component';
->>>>>>> 4995dd6d4829bed0cfd743ee9fa01c39abdb373e
 
 
 @NgModule({
@@ -50,12 +46,8 @@ import { UserProfileComponent } from './users/user-profile/user-profile.componen
     StarRatingComponent,
     AddCommentComponent,
     WelcomeComponent,
-<<<<<<< HEAD
     LoginPopupComponent,
-||||||| merged common ancestors
-=======
     UserProfileComponent,
->>>>>>> 4995dd6d4829bed0cfd743ee9fa01c39abdb373e
   ],
   imports: [
     BrowserModule,
