51fbeb6561295c1a892c686cafd834476d732b3f
Jinru Shi <qwe552325145@163.com>
2019-04-05 17:04:40 -0500

    user profile.

--- client/src/app/users/user-profile/user-profile.component.ts
+++ client/src/app/users/user-profile/user-profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
+import { UserService } from 'src/app/services/user.service';
+import { User } from 'src/app/models/user.model';
 
 @Component({
   selector: 'app-user-profile',
@@ -6,10 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-profile.component.css']
 })
 export class UserProfileComponent implements OnInit {
+  curUser: User=new User("","","","","",true,[]);
+  token:any;
 
-  constructor() { }
+  constructor(private userService : UserService,
+
+    ) { }
 
   ngOnInit() {
+    //open user info and wish list
+    $('#btn-wish-list').trigger("click");
+    $('#btn-user-info').trigger("click");
+
+    //send token to back end and get info of user
+    this.token = window.sessionStorage.getItem('mean-token');  
+    this.userService.getLoginedUser(this.token)
+      .subscribe(data =>
+      {
+        console.log(data);
+        
+        this.curUser.username=data['name'];
+        this.curUser.email=data['email'];
+        this.curUser.image=data['image'];
+      }); 
   }
 
+  //add new function here
 }
