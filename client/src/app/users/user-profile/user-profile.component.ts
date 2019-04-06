366d8b52608b1fa05c8038212463008ee25d3a97
Jinru Shi <qwe552325145@163.com>
2019-04-06 13:38:25 -0500

    userprofile/wishlist update

--- client/src/app/users/user-profile/user-profile.component.ts
+++ client/src/app/users/user-profile/user-profile.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import * as $ from 'jquery';
 import { UserService } from 'src/app/services/user.service';
 import { User } from 'src/app/models/user.model';
-// import { Movie } from 'src/app//models/movie.model';
+import { MoviesService } from 'src/app/services/movies.service';
 
 @Component({
   selector: 'app-user-profile',
@@ -17,7 +17,7 @@ export class UserProfileComponent implements OnInit {
   haveWishList: boolean = true;
 
   constructor(private userService : UserService,
-
+    private moviesService : MoviesService,
     ) { }
 
   ngOnInit() {
@@ -27,22 +27,77 @@ export class UserProfileComponent implements OnInit {
     $('#btn-history').trigger("click");
 
     //send token to back end and get info of user
-    this.token = window.sessionStorage.getItem('mean-token');  
+    this.token = this.userService.getToken()  
     this.userService.getLoginedUser(this.token)
       .subscribe(data =>
       {
         console.log(data);
-        this.curUser.username=data['name'];
-        this.curUser.email=data['email'];
-        this.curUser.image=data['image'];
-        this.wishList=data['list'];
-        if(this.wishList.length<=8){
-          this.havePaginate=false;
+        if(data['status']=="true")
+        {
+          this.curUser.username=data['name'];
+          this.curUser.email=data['email'];
+          this.curUser.image=data['image'];
+          this.wishList=data['list'];
+          if(this.wishList.length<=8){
+            this.havePaginate=false;
+          }
+          if(this.wishList.length==0){
+            this.haveWishList=false;
+          }
         }
-        if(this.wishList.length==0){
-          this.haveWishList=false;
+        else{
+          //to add a popup window
         }
       }); 
+  }
+
+  //delete movie from user wish list
+  public deleteFormUserWishList(event){
+    var target = event.target || event.srcElement || event.currentTarget;
+    var idAttr = target.attributes.id;
+    var movieId = idAttr.nodeValue;
+    const updateData = {
+      id : movieId,
+      token : this.token,
+      type: "wishlist",
+      value: "false"
+    }
+    this.moviesService.updateMoiveById(updateData)
+    .subscribe((updatedRes)=>{
+      if(updatedRes['status'] == "true"){
+        console.log("Successfully remove to user's movie list");
+        this.userService.getLoginedUser(this.token)
+        .subscribe(data =>
+        {
+          console.log(data);
+          if(data['status']=="true")
+          {
+            this.curUser.username=data['name'];
+            this.curUser.email=data['email'];
+            this.curUser.image=data['image'];
+            this.wishList=data['list'];
+            if(this.wishList.length<=8){
+              this.havePaginate=false;
+            }
+            if(this.wishList.length==0){
+              this.haveWishList=false;
+            }
+          }
+          else{
+            //to add a popup window
+          }
+        }); 
+      }
+      else{
+        //to be deleted
+        console.log(updatedRes['message']);
+      }
+    }); 
+  }
+  
+  //
+  trackingFunction(index:number, item:Object) : number {
+    return item["value"]
   }
 
   //add new function here
