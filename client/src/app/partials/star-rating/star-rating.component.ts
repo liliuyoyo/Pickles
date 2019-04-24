0fd32afdabd4547ef7c50664c9847b7b38c3be1d
Jinru Shi <qwe552325145@163.com>
2019-04-23 20:42:15 -0500

    rating login test update

--- client/src/app/partials/star-rating/star-rating.component.ts
+++ client/src/app/partials/star-rating/star-rating.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Input } from '@angular/core';
 import { UserService } from 'src/app/services/user.service';
 import { MoviesService } from 'src/app/services/movies.service';
 import * as $ from 'jquery';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+import { LoginPopupComponent } from 'src/app/users/login-popup/login-popup.component';
 
 @Component({
   selector: 'app-star-rating',
@@ -17,29 +19,40 @@ export class StarRatingComponent implements OnInit {
     emptyStars:0
   };
   rated: boolean = false;
-
   @Input() movieId: string;
+  modalRef: BsModalRef;
 
   constructor(private userService : UserService,
     private moviesService : MoviesService,
+    private modalService: BsModalService,
     ) { }
 
   ngOnInit() {
-    //send token to back end and get info of user
-    this.token = this.userService.getToken();
   }
   public listeners(event) {
-    var target = event.target || event.srcElement || event.currentTarget;
-    var idAttr = target.attributes.id;
-    var value = idAttr.nodeValue;
-    this.userStars= value;
-    const updateData = {
-      id : this.movieId,
-      token : this.token,
-      type: "rating",
-      value: this.userStars,
-    }
-    this.uploadRating(updateData);
+    this.userService.isLoggedIn()
+    .subscribe((res)=>{
+      // if user is loggedin
+      if(res=="true"){
+        this.token = this.userService.getToken();
+        var target = event.target || event.srcElement || event.currentTarget;
+        var idAttr = target.attributes.id;
+        var value = idAttr.nodeValue;
+        this.userStars= value;
+        const updateData = {
+          id : this.movieId,
+          token : this.token,
+          type: "rating",
+          value: this.userStars,
+        }
+        this.uploadRating(updateData);
+      }
+      else{
+        // if user is not loggedin , show popup login page
+        this.modalRef = this.modalService.show(LoginPopupComponent);
+      }
+    });
+    
   }
 
   public uploadRating(updateData:any){
